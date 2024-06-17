b755c8c1717ccb24b2c017be58e962466b0c4550
Ruben Schnell <r.h.schnell@student.tudelft.nl>
2024-06-17 11:36:53 +0200

    Resolve "Add confirmation when environment is saved"

--- Paper_VR/Assets/Scripts/OurCode/Environment/Environment/SaveController.cs
+++ Paper_VR/Assets/Scripts/OurCode/Environment/Environment/SaveController.cs
@@ -1,5 +1,7 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// This class contains the logic for the save button.
@@ -12,18 +14,50 @@ public class SaveController : MonoBehaviour
     public float savingTime = 45f;
 
     /// <summary>
+    /// The button for saving.
+    /// </summary>
+    public Button saveButton;
+
+    /// <summary>
+    /// The TMP_Text containing the save environment text.
+    /// </summary>
+    public TMP_Text saveMenuName;
+
+    /// <summary>
+    /// The TMP_Text containing the confirmation message shown when saving.
+    /// </summary>
+    public TMP_Text saveMenuConfirmation;
+
+    /// <summary>
+    /// An instance of GoogleMethods used for testing.
+    /// </summary>
+    public GoogleMethods googleMethods;
+
+    /// <summary>
     /// This method saves the environment information when the save button is clicked.
     /// </summary>
     public void Save()
     {
-        // Find the game object called GameManeger
+        // Find the game object called GameManager
         GameObject gameManager = GameObject.Find("GameManager");
 
         // Get the GameManager component with the environment script
         EnvironmentController environment = gameManager.GetComponent<EnvironmentController>();
 
         // Save the environment information
-        this.StartCoroutine(environment.SaveEnvironment(new GoogleMethods()));
+        this.StartCoroutine(environment.SaveEnvironment(this.googleMethods));
+    }
+
+    /// <summary>
+    /// Saves the environment when the user clicks the button.
+    /// </summary>
+    public void OnSaveClick()
+    {
+        // Save the environment
+        this.Save();
+
+        // Toggle the visibility of the save button to prevent spam clicking
+        this.StartCoroutine(this.ToggleConfirmation());
     }
 
     /// <summary>
@@ -40,8 +74,9 @@ public class SaveController : MonoBehaviour
     /// </summary>
     public void Start()
     {
-        // Cal lthe SaveEveryWhile method to enable autosave
+        // Call the SaveEveryWhile method to enable autosave
         this.StartCoroutine(this.SaveEveryWhile());
+        this.googleMethods = new GoogleMethods();
     }
 
     /// <summary>
@@ -52,9 +87,28 @@ public class SaveController : MonoBehaviour
     {
         // Wait 45 seconds before saving the environment
         yield return new WaitForSeconds(this.savingTime);
-        this.Save();
 
-        // Makes a call to itself
+        this.Save();
         this.StartCoroutine(this.SaveEveryWhile());
+    }
+
+    /// <summary>
+    /// Shows the confirmation message for 5 seconds to prevent spam clicking.
+    /// </summary>
+    /// <returns>The IEnumerator.</returns>
+    IEnumerator ToggleConfirmation()
+    {
+        // Show the confirmation message and set the button to inactive
+        this.saveButton.interactable = false;
+        this.saveMenuName.gameObject.SetActive(false);
+        this.saveMenuConfirmation.gameObject.SetActive(true);
+
+        // Wait for 5 seconds
+        yield return new WaitForSeconds(5.0f);
+
+        // Set the save button to be active and unshow the confirmation message
+        this.saveButton.interactable = true;
+        this.saveMenuName.gameObject.SetActive(true);
+        this.saveMenuConfirmation.gameObject.SetActive(false);
     }
 }
--- Paper_VR/Assets/Scripts/Tests/Environment/Environment/SaveButtonTests.cs
+++ Paper_VR/Assets/Scripts/Tests/Environment/Environment/SaveButtonTests.cs
@@ -1,8 +1,12 @@
 using System.Collections;
+using System.Threading.Tasks;
 using Moq;
 using NUnit.Framework;
+using TMPro;
 using UnityEngine;
 using UnityEngine.TestTools;
+using UnityEngine.UI;
+using UnityGoogleDrive;
 
 /// <summary>
 /// Contains unit tests for the <see cref="SaveController"/> class.
@@ -12,7 +16,7 @@ public class SaveButtonTests
     private GameObject gameManager;
     private GameObject saveButtonObject;
     private SaveController saveButton;
-    private Mock<EnvironmentController> environmentMock;
+    private Mock<GoogleMethods> googleMock;
 
     /// <summary>
     /// Sets up the test environment by creating necessary GameObjects and components.
@@ -22,25 +26,95 @@ public class SaveButtonTests
     {
         // Create a GameManager object and attach a mock EnvironmentController component
         this.gameManager = new GameObject("GameManager");
-        this.environmentMock = new Mock<EnvironmentController>();
-        this.gameManager.AddComponent<EnvironmentController>();
+        this.googleMock = new Mock<GoogleMethods>();
 
-        // Create a SaveController object and attach SaveController component
-        this.saveButtonObject = new GameObject("SaveController");
+        // this.gameManager.AddComponent<EnvironmentInformation>();
+
+        // Create real GameObjects for TMP_Text components
+        GameObject saveMenuNameObject = new GameObject("SaveMenuName");
+        TextMeshProUGUI saveMenuName = saveMenuNameObject.AddComponent<TextMeshProUGUI>();
+
+        GameObject saveMenuConfirmationObject = new GameObject("SaveMenuConfirmation");
+        TextMeshProUGUI saveMenuConfirmation = saveMenuConfirmationObject.AddComponent<TextMeshProUGUI>();
+
+        // Create SaveButton GameObject and attach SaveButton component and Button component
+        this.saveButtonObject = new GameObject("SaveButton");
         this.saveButton = this.saveButtonObject.AddComponent<SaveController>();
+        this.saveButton.saveButton = this.saveButtonObject.AddComponent<Button>();
 
-        // Optionally set the savingTime if you want to test the interval-based saving
-        this.saveButton.savingTime = 1f; // Set a low value for quicker test
+        // Assign real TextMeshProUGUI components to SaveButton
+        this.saveButton.saveMenuName = saveMenuName;
+        this.saveButton.saveMenuConfirmation = saveMenuConfirmation;
+
+        // Assign the mock GoogleMethods
+        this.saveButton.googleMethods = this.googleMock.Object;
+
+        // Set the savingTime to 1 second instead of 45 to make testing faster
+        this.saveButton.savingTime = 1f;
     }
 
     /// <summary>
-    /// Cleans up the test environment by destroying the created GameObjects.
+    /// Clean up the test environment.
     /// </summary>
     [TearDown]
     public void TearDown()
     {
-        // Clean up objects after tests
-        Object.Destroy(this.gameManager);
-        Object.Destroy(this.saveButtonObject);
+        // Clean up GameObjects
+        GameObject.Destroy(this.gameManager);
+        GameObject.Destroy(this.saveButtonObject);
+    }
+
+    /// <summary>
+    /// Tests if the Save method calls the FindEnviormentId method.
+    /// </summary>
+    [Test]
+    public void TestSave()
+    {
+        // Call the Save() method to act like the autosave method has saved the environment
+        this.saveButton.Save();
+
+        // Verify that the Save() method has saved the environment
+        this.googleMock.Verify(x => x.FindEnviormentId("New Environment", It.IsAny<GoogleDriveFiles.ListRequest>()));
+    }
+
+    /// <summary>
+    /// Tests if the environment is saved after quitting the application.
+    /// </summary>
+    [Test]
+    public void TestOnApplicationQuick()
+    {
+        // Call the OnApplicationQuit() method to act like we have quitted the application
+        this.saveButton.OnApplicationQuit();
+
+        // Verify that the Save() method has saved the environment
+        this.googleMock.Verify(x => x.FindEnviormentId("New Environment", It.IsAny<GoogleDriveFiles.ListRequest>()));
+    }
+
+    /// <summary>
+    /// Tests if the manual saves makes the save button not interactable.
+    /// </summary>
+    [Test]
+    public void TestOnSaveClick()
+    {
+        this.googleMock.Setup(x => x.FindEnviormentId(It.IsAny<string>(), It.IsAny<GoogleDriveFiles.ListRequest>()))
+                  .Returns(this.FakeEnumerator());
+
+        // Call OnSaveClick() method to act like we have manually saved the environment
+        this.saveButton.OnSaveClick();
+
+        // Verify that the Save() method has saved the environment
+        this.googleMock.Verify(x => x.FindEnviormentId("New Environment", It.IsAny<GoogleDriveFiles.ListRequest>()));
+        Assert.IsFalse(this.saveButton.saveButton.interactable);
+        Assert.IsFalse(this.saveButton.saveMenuName.IsActive());
+        Assert.IsTrue(this.saveButton.saveMenuConfirmation.IsActive());
+    }
+
+    /// <summary>
+    /// Returns an IEnumerator used for mocking.
+    /// </summary>
+    /// <returns>An IEnumerator.</returns>
+    private IEnumerator FakeEnumerator()
+    {
+        yield return null;
     }
 }
